7f03cdfaf89ae01a9fcdd24212c3dbd888b4ee73
babitatandi26 <babitatandi266@gmail.com>
2020-08-24 14:19:08 +0530

    Updates

---
+++ precache-manifest.21264b42a2179863b81aeced7750d2d7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcdfd3a2b86a789c9be4ad269451953",
+    "url": "/b-blog/index.html"
+  },
+  {
+    "revision": "9e7620bd27f705250380",
+    "url": "/b-blog/static/css/main.cee57ce8.chunk.css"
+  },
+  {
+    "revision": "0b8c20e3e2419a33cbeb",
+    "url": "/b-blog/static/js/2.be84e439.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/b-blog/static/js/2.be84e439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7620bd27f705250380",
+    "url": "/b-blog/static/js/main.7e86e267.chunk.js"
+  },
+  {
+    "revision": "73932973f0fdb58d27bc",
+    "url": "/b-blog/static/js/runtime-main.3e7e09e2.js"
+  },
+  {
+    "revision": "ff4f8a305eccfcfd3001d2f1725c031d",
+    "url": "/b-blog/static/media/01.ff4f8a30.jpg"
+  },
+  {
+    "revision": "14b9456cae209a45580922f282aaa91b",
+    "url": "/b-blog/static/media/02.14b9456c.jpg"
+  },
+  {
+    "revision": "db511a857d52b7a68d4be26acee21a97",
+    "url": "/b-blog/static/media/03.db511a85.jpg"
+  },
+  {
+    "revision": "fbd9732abc6c01c90c21a042a6632b21",
+    "url": "/b-blog/static/media/04.fbd9732a.jpg"
+  },
+  {
+    "revision": "616f3d7c5512c81562bedd44a6aa444e",
+    "url": "/b-blog/static/media/05.616f3d7c.jpg"
+  },
+  {
+    "revision": "ec6cde0da3cd81f50d8e69016d843747",
+    "url": "/b-blog/static/media/06.ec6cde0d.jpg"
+  },
+  {
+    "revision": "31713340501904124ec534f5d4703f45",
+    "url": "/b-blog/static/media/07.31713340.jpg"
+  },
+  {
+    "revision": "5a6428d1ccdfe447be673ce6dfffd932",
+    "url": "/b-blog/static/media/08.5a6428d1.jpg"
+  },
+  {
+    "revision": "ad3f365dc25aa72d3c3a6b9a0c274908",
+    "url": "/b-blog/static/media/1.ad3f365d.jpg"
+  },
+  {
+    "revision": "68f36c071b3c594589d04acfe3a48860",
+    "url": "/b-blog/static/media/2.68f36c07.jpg"
+  },
+  {
+    "revision": "ed25bc748003d8022187c2cd840c6a5d",
+    "url": "/b-blog/static/media/3.ed25bc74.jpg"
+  },
+  {
+    "revision": "3c6a841cd05c86f33ab87739e7f753be",
+    "url": "/b-blog/static/media/4.3c6a841c.jpg"
+  },
+  {
+    "revision": "c4d22db8e2f83234565537f5cb1ba7f9",
+    "url": "/b-blog/static/media/5.c4d22db8.jpg"
+  },
+  {
+    "revision": "910bb682261b18bc3e45d5b6891c4f00",
+    "url": "/b-blog/static/media/6.910bb682.jpg"
+  },
+  {
+    "revision": "b2cf6c37cfa2cc6d9bcf1e3f28a73f73",
+    "url": "/b-blog/static/media/7.b2cf6c37.jpg"
+  }
+]);
